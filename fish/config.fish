148f28e58a6719e17cd4c4133fba460af300e2f0
zteezy19 <zteezy.dev@gmail.com>
2025-01-02 10:22:25 +0800

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,39 +1,35 @@
 set -g fish_greeting
 
 if status is-interactive
+    # Tool initializations
     starship init fish | source
+    zoxide init fish | source
 
-    # Set default terminal
-    # set -x TERMINAL ghostty
-    # set -x TERMINAL kitty
-
+    # Environment settings
     set -gx EDITOR nvim
     set -gx VISUAL nvim
+    # set -x TERMINAL ghostty  # Uncomment whichever terminal you want
+    # set -x TERMINAL kitty
+
+    # Directory listing aliases
+    alias l='eza -lh  --icons=auto'
+    alias ls='eza -1   --icons=auto'
+    alias ll='eza -lha --icons=auto --sort=name --group-directories-first'
+    alias ld='eza -lhD --icons=auto'
+    alias lt='eza --icons=auto --tree'
+
+    # Directory navigation
+    abbr .. 'cd ..'
+    abbr ... 'cd ../..'
+    abbr .3 'cd ../../..'
+    abbr .4 'cd ../../../..'
+    abbr .5 'cd ../../../../..'
+
+    # System commands
+    abbr mkdir 'mkdir -p'
+    alias install='sudo pacman -S'
+    alias sysupdate='sudo pacman -Syu'
+    alias search='sudo pacman -Ss'
+    alias conf='cd .config'
+    alias nvim='bash ~/.config/kitty/kitty.sh'
 end
-
-# List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
-
-# Handy change dir shortcuts
-abbr .. 'cd ..'
-abbr ... 'cd ../..'
-abbr .3 'cd ../../..'
-abbr .4 'cd ../../../..'
-abbr .5 'cd ../../../../..'
-
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
-abbr mkdir 'mkdir -p'
-
-alias nvim='bash ~/.config/kitty/kitty.sh'
-
-zoxide init fish | source
-
-# QOL pacman scripts
-alias install='sudo pacman -S'
-alias sysupdate='sudo pacman -Syu'
-alias search='sudo pacman -Ss'
-alias conf='cd .config'
