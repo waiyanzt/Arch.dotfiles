6132690b09f21cf88f974e3be2d6be445da9d8ec
waiyanzt01 <zteezy.dev@gmail.com>
2025-09-02 05:08:42 +0800

    Updated nvim config

---
+++ nvim/lua/themes.lua
@@ -0,0 +1,37 @@
+return {
+  -- Rose Pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {},
+  },
+
+  -- Zenbones
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    priority=1000,
+    config = function()
+      vim.g.zenbones_darken_comments = 50
+      vim.g.zenbones_dark_mode = true -- Dark version
+      vim.cmd.colorscheme("kanagawabones")
+    end,
+  },
+
+  -- Eva-01
+  -- {
+  --   "hachy/eva01.vim",
+  --   opts = {},
+  --     -- or
+  --     -- vim.cmd.colorscheme "eva01-LCL"
+  
+
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- mocha is the dark variant
+    },
+  },
+}
